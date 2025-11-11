237efbc6044f65142d1ec88a89f42072f4dbef69
Olav <wltwt@proton.me>
2025-11-11 18:33:44 +0100

    Quartus skeleton and gitignore

---
+++ FPGA/rtl/u_baudgen.vhd
@@ -0,0 +1,18 @@
+library ieee; 
+use ieee.std_logic_1164.all;
+
+entity u_baudgen is
+	port (
+		clk		:	in  std_logic;
+		rst_n		:	in  std_logic;
+		tick		:	out std_logic
+	);
+end entity;
+
+
+architecture rtl of u_baudgen is
+begin
+
+
+
+end architecture;
---
+++ FPGA/sim/README.md
@@ -0,0 +1,1 @@
+# Add Modelsim Testbench here
